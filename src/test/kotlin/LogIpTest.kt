68c9353a6c41b3cb5c50fb3566f1ddd9f7606397
Jason Ernst <ernstjason1@gmail.com>
2024-11-20 13:18:44 -0800

    lint fix

--- src/test/kotlin/LogIpTest.kt
+++ src/test/kotlin/LogIpTest.kt
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test
 
 class LogIpTest {
-
     // todo: make a test that ads a lo filter and ensures that the loopback ip isn't included
 
     @Test fun testLogIp() {
